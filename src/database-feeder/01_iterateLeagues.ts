862cab46f6d7f444cadc7ca22198a2df38469a67
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-11-16 14:01:32 +0100

    refactor(DatabaseFeeder): :fire: remove old workflow version

--- src/database-feeder/01_iterateLeagues.ts
+++
@@ -1,6 +0,0 @@
-import { lolESportsAPI } from "../external-services/lol-esports-api/lolESportsAPI.ts";
-
-export async function* iterateLeagues() {
-  const { data: { leagues } } = await lolESportsAPI.leagues.get();
-  yield* leagues;
-}
