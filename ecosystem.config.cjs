602d6a9edc7a7f64e9507d729584bb35e51cb80a
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-11-17 22:25:41 +0100

    build: specify node22

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ exports.apps = [
   {
     name: "lol-esports-clips:server",
     script: "src/server/server.ts",
-    interpreter: "node",
+    interpreter: "node22",
     interpreter_args: "--env-file=.env",
   },
 ];
